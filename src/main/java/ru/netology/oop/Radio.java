0a884ed8aa6b7434bdc58cbf2c99d17aae06fea2
Anastasia Fedulova <Fedulova.a.m@bk.ru>
2025-01-23 04:16:17 +0300

    added method increaseVolume

--- src/main/java/ru/netology/oop/Radio.java
+++ src/main/java/ru/netology/oop/Radio.java
@@ -1,7 +1,7 @@
 package ru.netology.oop;
 
 public class Radio {
-    public int currentStationNumber;
+    private int currentStationNumber;
     private int currentVolume;
 
     public int getCurrentStationNumber() {
@@ -19,20 +19,18 @@ public class Radio {
     }
 
     public int next() {   // переключает радио станцию вперед
-        if (currentStationNumber == 9) {
+        if (currentStationNumber != 9) {
+            currentStationNumber++;
+        } else {
             currentStationNumber = 0;
-        }
-        if (currentStationNumber > 0) {
-            currentStationNumber = currentStationNumber + 1;
         }
         return currentStationNumber;
     }
 
     public int prev() {    // переключает радио станцию назад
-        if (currentStationNumber > 0) {
-            currentStationNumber = currentStationNumber - 1;
-        }
-        if (currentStationNumber == 0) {
+        if (currentStationNumber != 0) {
+            currentStationNumber--;
+        } else {
             currentStationNumber = 9;
         }
         return currentStationNumber;
@@ -43,19 +41,41 @@ public class Radio {
     }
 
     public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume > 100) {
+            return;
+        }
+        if (newCurrentVolume < 0) {
+            return;
+        }
         currentVolume = newCurrentVolume;
     }
 
     public void louder() {    // повышает громкость на 1
         if (currentVolume < 100) {
-            currentVolume = currentVolume + 1;
+            currentVolume++;
         }
     }
 
     public void quieter() {    // понижает громкость на 1
         if (currentVolume > 0) {
-            currentVolume = currentVolume - 1;
+            currentVolume--;
         }
     }
 
+    public void increaseVolume(int volume) { // увеличивает до нужной громкости
+        int target = currentVolume;
+
+        if (volume > 0) {
+            while (target < volume) {
+                louder();
+                target++;
+            }
+        } else {
+            while (target > volume) {
+                quieter();
+                target--;
+            }
+        }
+        setCurrentVolume(target);
+    }
 }
--- src/test/java/ru/netology/oop/RadioTest.java
+++ src/test/java/ru/netology/oop/RadioTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 public class RadioTest {
 
+// граничные значения ввода радиостанции ( 10, -1,)
+
     @Test
     public void shouldInsertRadioStationAboveMax() {    // включим станцию выше максимальной
         Radio radio = new Radio();
@@ -29,8 +31,10 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
+// границные значения Next (=max, !=max, 0) + граничные значения ввода номера станции ( 1, 9, 0)
+
     @Test
-    public void shouldSwitchToNextRadioStationFromFirst() {    // включим станцию в диапазоне (с 1) и нажмем Next
+    public void shouldSwitchToNextRadioStationFromFirst() {    // нажмем Next находясь на 1 станции
         Radio radio = new Radio();
 
         radio.setCurrentStationNumber(1);
@@ -41,26 +45,44 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
+
     @Test
     public void shouldNotSwitchToNextRadioStationAboveMax() {   // нажмем Next находясь на максимальной станции
         Radio radio = new Radio();
 
         radio.setCurrentStationNumber(9);
+        radio.next();
 
         int expected = 0;
-        int actual = radio.next();
+        int actual = radio.getCurrentStationNumber();
 
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldSwitchToPrevRadioStationFromEighth() {   // включим станцию в диапазоне (c 8) и нажмем Prev
+    public void shouldSwitchToNextRadioStationFromMin() {   // нажмем Next находясь на минимальной станции
+        Radio radio = new Radio();
+
+        radio.setCurrentStationNumber(0);
+        radio.next();
+
+        int expected = 1;
+        int actual = radio.getCurrentStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+// // границные значения Prev (=min, !=min, 0) + граничные значения ввода номера станции ( 8, 9, 0)
+
+    @Test
+    public void shouldSwitchToPrevRadioStationFromEighth() {   // нажмем Prev находясь на 8 станции
         Radio radio = new Radio();
 
         radio.setCurrentStationNumber(8);
+        radio.prev();
 
         int expected = 7;
-        int actual = radio.prev();
+        int actual = radio.getCurrentStationNumber();
 
         Assertions.assertEquals(expected, actual);
     }
@@ -70,18 +92,21 @@ public class RadioTest {
         Radio radio = new Radio();
 
         radio.setCurrentStationNumber(0);
+        radio.prev();
 
         int expected = 9;
-        int actual = radio.prev();
+        int actual = radio.getCurrentStationNumber();
 
         Assertions.assertEquals(expected, actual);
     }
 
+// верхние граничные значения увеличение громкости (99, 100, 101)
+
     @Test
-    public void shouldIncreaseVolumeWithinLimit() {  // увеличени звука в существующем диапазоне
+    public void shouldIncreaseVolumeWithinLimit() {  // увеличение звука в существующем диапазоне
         Radio radio = new Radio();
 
-        radio.setCurrentVolume(99);
+        radio.increaseVolume(99);
         radio.louder();
 
         int expected = 100;
@@ -94,7 +119,7 @@ public class RadioTest {
     public void shouldNotIncreaseVolumeAboveMax() {  // не должен увеличить звук выше максимального
         Radio radio = new Radio();
 
-        radio.setCurrentVolume(100);
+        radio.increaseVolume(100);
         radio.louder();
 
         int expected = 100;
@@ -104,10 +129,39 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldDecreaseVolumeWithinLimit() {  // понижение звука в существующем диапазоне
+    public void shouldNotSwitchVolumeAboveMax() {  // не должен увеличить звук выше максимального
         Radio radio = new Radio();
 
-        radio.setCurrentVolume(1);
+        radio.increaseVolume(101);
+        radio.louder();
+
+        int expected = 100;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // нижние граничные значения уменьшения громкости (-1, 1, 0)
+
+    @Test
+    public void shouldNotSwitchVolumeUnderMin() {  // не должен переключать звук ниже минимального
+        Radio radio = new Radio();
+
+        radio.increaseVolume(-1);
+        radio.quieter();
+
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecreaseVolumeWithinLimit() {  // понижение звука в существующем диапазоне ( c 1)
+        Radio radio = new Radio();
+
+        radio.increaseVolume(1);
         radio.quieter();
 
         int expected = 0;
@@ -120,7 +174,7 @@ public class RadioTest {
     public void shouldNotDecreaseVolumeUnderMin() {  // не должен понижать звук ниже минимального
         Radio radio = new Radio();
 
-        radio.setCurrentVolume(0);
+        radio.increaseVolume(0);
         radio.quieter();
 
         int expected = 0;
@@ -128,6 +182,4 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
-
-
 }
